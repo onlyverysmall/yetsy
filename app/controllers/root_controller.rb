9e94742c4f4c30d40d09f0bcc61347d615125ebe
Sarah Hadley <sarah@onlyverysmall.com>
2013-07-03 21:31:11 -0700

    Fix favorites on index if current_user is nil

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,6 +3,8 @@ class RootController < ApplicationController
     # will eventually need to paginate this
     @items = Item.all
     @categories = Category.all
-    @favorited_items = current_user.favorited_items
+    if current_user
+      @favorited_items = current_user.favorited_items 
+    end
   end
 end
